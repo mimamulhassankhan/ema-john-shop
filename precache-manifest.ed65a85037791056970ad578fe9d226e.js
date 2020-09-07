092891e16f1745873cbf924a0c3288743081d53c
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-07 16:25:45 +0600

    Updates

---
+++ precache-manifest.ed65a85037791056970ad578fe9d226e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a0c51d92c7fe527d3e83cfa66058a7",
+    "url": "/ema-john-shop/index.html"
+  },
+  {
+    "revision": "a821a094397e4bf3d815",
+    "url": "/ema-john-shop/static/css/main.8fe99579.chunk.css"
+  },
+  {
+    "revision": "e578b0fa12ab0fd838e9",
+    "url": "/ema-john-shop/static/js/2.5651f1b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ema-john-shop/static/js/2.5651f1b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a821a094397e4bf3d815",
+    "url": "/ema-john-shop/static/js/main.d756ee5e.chunk.js"
+  },
+  {
+    "revision": "386d97067b7ebfa45197",
+    "url": "/ema-john-shop/static/js/runtime-main.6e320c54.js"
+  },
+  {
+    "revision": "95f238a51a560680815e09e999af5cb9",
+    "url": "/ema-john-shop/static/media/logo.95f238a5.png"
+  }
+]);
