66574e3b31dea9f01aac58a23f43a75c851024ff
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-17 11:24:56 +0600

    refactoring worked

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css'
 import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Header = () => {
     return (
@@ -11,6 +12,7 @@ const Header = () => {
                 <Link to="/shop">Shop</Link>
                 <Link to="/review">Review Cart</Link>
                 <Link to="/manage">Manage Shop</Link>
+                <Button className="mx-auto" variant="danger">Sign Out</Button>
             </nav>
         </div>
     );
