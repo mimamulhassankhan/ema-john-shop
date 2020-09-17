31acb993ebe1172a89e998cb1f1ab4693d276bcb
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-17 12:08:59 +0600

    shipment added

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css'
 import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { UserContext } from '../../App';
 
 const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     return (
         <div className="header">
             <img src={logo} alt="logo"/>
@@ -12,7 +14,7 @@ const Header = () => {
                 <Link to="/shop">Shop</Link>
                 <Link to="/review">Review Cart</Link>
                 <Link to="/manage">Manage Shop</Link>
-                <Button className="mx-auto" variant="danger">Sign Out</Button>
+                <Button onClick={() => setLoggedInUser({})} className="mx-auto" variant="danger">Sign Out</Button>
             </nav>
         </div>
     );
--- src/Component/Shipment/Shipment.js
+++ src/Component/Shipment/Shipment.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { UserContext } from '../../App';
 
 const Shipment = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
+
+    console.log(watch("example")); 
+
     return (
-        <div>
-            <h1>This is shipment</h1>
-        </div>
-    );
-};
+
+        <form className="form-group" onSubmit={handleSubmit(onSubmit)} placeholder="Enter name">
+            <input className="form-control" name="name" defaultValue={loggedInUser.name} ref={register({ required: true })} />
+            {errors.name && <span>Name is required</span>}
+
+            <br/>
+            <input className="form-control" name="email" defaultValue={loggedInUser.email} ref={register({ required: true })}  placeholder="Enter email"/>
+            {errors.email && <span>Email is required</span>}
+
+            <br/>
+            <input className="form-control" name="address" ref={register({ required: true })} placeholder="Enter address"/>
+            {errors.address && <span>Address is required</span>}
+            
+            <br/>
+            <input className="form-control" name="phone" ref={register({ required: true })} placeholder="Enter phone"/>
+            {errors.phone && <span>Phone is required</span>}
+            
+            <br/>
+            <input type="submit" />
+        </form>);
+}
 
 export default Shipment;
